339f9be55e2220173d1fef0f0179cb800215c8e9
Etai Nativ <etainativ@gmail.com>
2024-04-30 09:54:17 +0300

    Seperating logic to engine and renderer

--- engine/engine.h
+++ engine/engine.h
@@ -1,106 +1,16 @@
-#pragma once
-
+#include "renderer.h"
 #include "scene.h"
-#include <vulkan/vulkan_core.h>
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-
-#include "vk_mem_alloc.h"
-#include <vector>
-#include <deque>
-#include <functional>
-
-constexpr int FRAME_OVERLAP = 2;
-
-
-struct FrameData {
-    VkCommandBuffer commandBuffer;
-    VkCommandPool commandPool;
-    VkSemaphore swapchainSemaphore, renderSemaphore;
-    VkFence renderFence;
-};
-
-struct Image {
-    VkImage image;
-    VkImageView view;
-    VmaAllocation allocation;
-    VkExtent3D imageExtent;
-    VkFormat imageFormat;
-};
-
 
 class Engine {
     public:
 	Engine();
 	~Engine();
-
-	VkDevice device;
-	VkSwapchainKHR swapchain;
-	VkFormat swapchainImageFormat;
-
-	VkAllocationCallbacks *pAllocator= nullptr;
-	VmaAllocator allocator;
-
-	std::vector<VkImage> swapchainImages;
-	std::vector<VkImageView> swapchainImageViews;
-	VkExtent2D swapchainExtent;
 	VkExtent2D windowExtent = { 1200, 960 };
-	bool bQuit = false;
-	void resize();
-	void run(Scene &FirstScene);
-
+	void run(Scene &firstScene);
+	void resize() {renderer->resize();}
     private:
-	int frameNumber = 0;
-
+	Renderer *renderer;
 	GLFWwindow *window;
-	VkInstance instance;
-	VkDebugUtilsMessengerEXT debugMessenger;
-	VkSurfaceKHR surface;
-	VkPhysicalDevice phyDevice = VK_NULL_HANDLE;
-
-	// Formats
-	VkFormat depthFormat;
-	VkFormat stencilFormat;
-
-	VkQueue graphicsQueue;
-	uint32_t graphicsQueueFamily;
-
-	FrameData frames[FRAME_OVERLAP];
-	Image drawImage;
-	Image depthImage;
-
-	std::deque<std::function<void()>> deletors;
-	void onDestruct(std::function<void()>&& function) {
-		deletors.push_back(function); }
-
-	// immediate submit structures
-	VkFence immFence;
-	VkCommandBuffer immCommandBuffer;
-	VkCommandPool immCommandPool;
-	void immediateSubmit(std::function<void(VkCommandBuffer cmd)>&& function);
-
-	// imGUI
-	VkDescriptorPool imguiPool;
-
-	void initWindow();
-	void createSurface();
-	void initVulkan();
-	void initFormats();
-	void initSwapchain();
-	void initFrameData();
-	void initImages();
-	void initImCommand();
-	void initImgui();
-	void destroySwapchain();
-	void draw(Scene& scene);
-	void presentImage(
-		VkCommandBuffer &cmd,
-		FrameData &currFrame,
-		uint32_t &swapchainImageIndex);
-	void loadScene(Scene &scene);
-	void unloadScene(Scene &scene);
-	void loadMesh(Mesh &mesh);
-	void unloadMesh(Mesh &mesh);
-	AllocatedBuffer create_buffer(size_t allocSize, VkBufferUsageFlags usage, VmaMemoryUsage memoryUsage);
 
 };
+
--- main.cpp
+++ main.cpp
@@ -33,8 +33,8 @@ int main() {
     scene.objects.push_back(obj);
 
     scene.camera = glm::lookAt(
-	    glm::vec3(0.0, 0.0, 1.0f),
 	    glm::vec3(0.0, 0.0, 0.0f),
+	    glm::vec3(0.0, 0.0, 1.0f),
 	    glm::vec3(0.0, 1.0, 0.0f));
 
     auto engine = Engine();
