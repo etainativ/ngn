50b7699331da7f0bf6c7336c2dcefd3aeaf89e46
Etai Nativ <etainativ@gmail.com>
2024-05-20 09:46:54 +0300

    protobufs

--- srcs/systems/client/networking.cpp
+++ srcs/systems/client/networking.cpp
@@ -1,27 +1,29 @@
 #include "engine/system.h"
 #include "networking/transport.h"
-#include "networking/rpc.h"
 #include "configuration/global.h"
 #include "time.h"
 
+#include "msgs.pb.h"
+#include "rpc.pb.h"
+#include <google/protobuf/arena.h>
+#include <string>
 #define MAX_INFLISHGT_TIME 5 * CLOCKS_PER_SEC
 
 Client *client;
-msgId_t __msgId = 10;
+uint32_t __msgId = 10;
 struct inFlightRPCInfo {
     clock_t timeSent;
-    Datagram dg;
+    NetworkRPCMessage *rpc;
 };
-std::map<msgId_t, inFlightRPCInfo> inFlightRPCMessages;
+std::map<uint32_t, inFlightRPCInfo> inFlightRPCMessages;
+google::protobuf::Arena rpcArena;
 
-
-void handleRPCMessage(void *msg) {
-    RPCBase *rpc = static_cast<RPCBase *>(msg);
-    switch (rpc->rpcType) {
-	case RPC_TYPE_SERVER_ACK:
-	    if (inFlightRPCMessages.find(rpc->msgId) != inFlightRPCMessages.end()) {
-		delete static_cast<RPCBase *>(inFlightRPCMessages[rpc->msgId].dg.data);
-		inFlightRPCMessages.erase(rpc->msgId);
+void handleRPCMessage(const NetworkRPCMessage &rpc) {
+    switch (rpc.networkRPCMessageType_case()) {
+	case NetworkRPCMessage::kServerack:
+	    if (inFlightRPCMessages.find(rpc.msgid()) != inFlightRPCMessages.end()) {
+		delete inFlightRPCMessages[rpc.msgid()].rpc;
+		inFlightRPCMessages.erase(rpc.msgid());
 	    }
 	    break;
 	default:
@@ -31,52 +33,60 @@ void handleRPCMessage(void *msg) {
 };
 
 
-void sendRPCData(Datagram &data, msgId_t msgId) {
-    clientSend(client, data);
-    inFlightRPCMessages[msgId] = {clock(), data};
-};
+void readMessages() {
+    Datagram data;
+    while (clientRecv(client, &data)) {
+	NetworkMessage msg;
+	msg.ParseFromArray(data.data, data.size);
+	switch (msg.networkMessageType_case()) {
+	    case NetworkMessage::kRpcMessage:
+		handleRPCMessage(msg.rpcmessage());
+		break;
+	    case NetworkMessage::kInputMessage:
+		// TODO client should not get input MESSAGE_TYPE_SNAPSHOT
+		// TODO log error
+		break;
+	    case NetworkMessage::kSnapshotMessage:
+		//handleSnapshotMessage(data.data);
+		break;
+	    case NetworkMessage::NETWORKMESSAGETYPE_NOT_SET:
+		// TODO log error
+		break;
+
+	}
+    }
+}
 
 
-template <class T>
-void sendRPC(T& rpc, msgId_t msgId) {
-    assert(client != nullptr);
-    T *msg = new T(rpc);
-    Datagram data = {
-	.size = sizeof(T),
-	.data = msg
-    };
-    dynamic_cast<RPCBase*>(msg)->msgId = msgId;
-    sendRPCData(data, msgId);
+void sendMsg(NetworkMessage &msg) {
+    std::string data = msg.SerializeAsString();
+    Datagram dg = {
+	.size = uint32_t(data.size()),
+	.data = (void *)data.c_str()};
+    clientSend(client, dg);
+}
+
+
+void sendRPC(NetworkRPCMessage *rpc, uint32_t msgId) {
+    rpc->set_msgid(msgId);
+    inFlightRPCMessages[msgId] = {clock(), rpc};
+    NetworkMessage msg;
+    msg.set_allocated_rpcmessage(rpc);
+    sendMsg(msg);
+    msg.release_rpcmessage();
+}
+
+
+void sendClientHello() {
+    NetworkRPCMessage *newRPC = new NetworkRPCMessage();
+    newRPC->mutable_clientconnect();
+    sendRPC(newRPC, __msgId++);
 }
 
 
 void initClientNetworkSystem(entt::registry *entities) {
     client = clientInit(HOSTNAME, SERVER_PORT, 0);
-    RPCClientConnect rpc = {};
-    sendRPC(rpc, __msgId++);
-}
-
-
-
-void readMessages() {
-    Datagram data;
-    while (clientRecv(client, &data)) {
-	BaseMessage *msg = static_cast<BaseMessage *>(data.data);
-	switch (msg->msgType) {
-	    case MESSAGE_TYPE_RPC:
-		handleRPCMessage(data.data);
-		break;
-	    case MESSAGE_TYPE_INPUTS:
-		// TODO client should not get input MESSAGE_TYPE_SNAPSHOT
-		// TODO log error
-		break;
-	    case MESSAGE_TYPE_SNAPSHOT:
-		//handleSnapshotMessage(data.data);
-		break;
-
-	}
-
-    }
+    sendClientHello();
 }
 
 
@@ -85,7 +95,7 @@ void retrySends() {
     for (auto &[msgId, msgInfo] : inFlightRPCMessages) {
 	if ((now - msgInfo.timeSent) > MAX_INFLISHGT_TIME)
 	    // TODO log
-	    sendRPCData(msgInfo.dg, msgId);
+	    sendRPC(msgInfo.rpc, msgId);
     }
 }
 
